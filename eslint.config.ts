520ea5ed788269c81ba0d4a90c720e931d4a78f3
Joao123433 <joaosantos123433@gmail.com>
2026-06-17 16:02:46 -0300

    chore: initialize monorepo tooling

---
+++ eslint.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    ignores: ['dist/**', 'build/**', 'node_modules/**'],
+  },
+  tseslint.configs.recommended,
+]);
